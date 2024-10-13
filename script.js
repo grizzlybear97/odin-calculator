5abe11f9cff106559ec8c4e619039a0b1a4dacb8
sandeep <grizzlybear986@gmail.com>
2024-10-13 17:22:48 +0530

    add display, button and clear function.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//global variables
+let firstValue;
+let secondValue;
+let output;
+
 //math functions
 add = (a,b) => a+b;
 sub = (a,b) => a-b;
@@ -21,5 +26,15 @@ console.log(operate(5,6,'*'))
 
 //function to store display info
 function display(){
-    
+    let buttons = document.querySelectorAll('button');
+    const display = document.querySelector('.display');
+    buttons = [...buttons];
+    buttons.map(button => button.addEventListener('click', () => {
+        if(button.innerText == 'C') {
+            console.log('clear pressed');
+            display.innerText = ''
+        }else display.innerText += button.innerText;
+    }))
 }
+
+display();
